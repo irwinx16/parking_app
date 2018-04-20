66f3fda70115267297df9683e1f8d66d59cc22dc
marietrull <marietrull@gmail.com>
2018-04-20 14:12:26 -0500

    Commit: Added spots model and controller

--- controllers/spots.js
+++ controllers/spots.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users.js')
+const Spot = require('../models/spots.js')
 
 //ROUTE TO MY SPOTS INDEX
 
-router.get('/', (req,res) => {
-	
-	res.render('spots/index.ejs');
-})
+router.get('/', async (req,res) => {
 
+	if(req.session.logged) {
+		try {
+			//await - wait for the db response
+			const theArticlesIFound = await Articles.find();
+
+			res.render('spots/index.ejs', {
+				articles: theArticlesIFound,
+				username: req.session.username
+			})
+
+		} catch (err){
+
+			res.send(err);
+
+		}
+	} else {
+		req.session.message = "Please Login."
+		res.redirect('/');
+	}
+});
 
 module.exports = router;
---
+++ models/spots.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+//how all of the objects in the collection will look
+const spotSchema = new mongoose.Schema({
+	spotname: String
+});
+
+//Creating the collection --> author will be the name of the collection
+module.exports = mongoose.model('Spots', userSchema);
