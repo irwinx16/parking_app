24698de7f99dfcb0cdf1be9915287ad856f35d63
Irwin <irgranados16@gmail.com>
2018-04-20 11:34:59 -0500

    json data working

--- controllers/home.js
+++ controllers/home.js
@@ -6,10 +6,10 @@ const router = express.Router();
 // 	res.render('home/home.ejs');
 
 
-let request = require('request');
-let url = 'https://data.cityofchicago.org/resource/ys7w-i4tk.json'
+const request = require('request');
+const url = 'https://data.cityofchicago.org/resource/ys7w-i4tk.json'
 request(url, (err, response, body) => {
-  let zones = JSON.parse(body);
+const zones = JSON.parse(body);
   console.log(zones[0].address_range_low);
 console.log(zones[0].address_range_high);
 console.log(zones[0].street_direction);
@@ -18,7 +18,6 @@ console.log(zones[0].street_type);
 console.log(zones[0].zone);
 
 });
-
 
 
 
