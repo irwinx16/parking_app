9d3ab3a0fc92dbc5c80e162edeb862242aa89d09
Sam <sammintz1993@gmail.com>
2018-04-20 10:07:33 -0500

    Google map with typed in location added in index file for testing

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: {lat: 41.8781, lng: -87.6298}
+    });
+    const geocoder = new google.maps.Geocoder();
+
+    document.getElementById('submit').addEventListener('click', function() {
+      geocodeAddress(geocoder, map);
+    });
+  }
+
+
+function geocodeAddress(geocoder, resultsMap) {
+    const address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        resultsMap.setCenter(results[0].geometry.location);
+        const marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+}
+
+
+
